a4f540122cff0cd8fde213903712020847902470
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-12-27 14:56:58 -0600

    Initial Commit

---
+++ AcroKnow/AcroKnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcroKnowApp.swift
+//  AcroKnow
+//
+//  Created by Renato Oliveira Fraga on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AcroKnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
